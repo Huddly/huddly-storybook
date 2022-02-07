252e234d4e4d097d8fd931c70bf3998f4a26dcba
Andrea Marie Festøy <andreamarie@huddly.com>
2022-02-07 13:53:32 +0100

    feat: Add text component

--- huddly-storybook/src/stories/Foundation/Text/index.tsx
+++ huddly-storybook/src/stories/Foundation/Text/index.tsx
@@ -1,32 +1,42 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-interface TextProps {
-  /**
-   * children description.
-   */
-  children: any;
-
-  /**
-   * bold description.
-   */
-  bold: boolean;
-
-  /**
-   * color description.
-   */
-  color: "black" | "purple";
-}
-
-export const Wrapper = styled.div`
-  /* Your styles here */
+export const Wrapper = styled.p<Props>`
+    margin: 0;
+    line-height: 1.2em;
+    font-size: ${(p) => `var(--font-size-${p.size})`};
+    color: ${(p) => `var(--color-${p.color})`};
+    font-family: ${(p) =>
+        p.bold ? 'Messina sans bold' : 'Messina sans regular'};
 `;
-
+interface Props {
+    size?: '14' | '18' | '22' | '28' | '48' | '68' | '98';
+    color?:
+        | 'lavender'
+        | 'sunYellow'
+        | 'black'
+        | 'signalOrange'
+        | 'white'
+        | 'signalOrange';
+    type?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'span';
+    bold?: boolean;
+    children: React.ReactChild;
+}
 /**
  * Text component
  */
-export const Text = ({ children, bold, color }: TextProps) => {
-  return <Wrapper>{children}</Wrapper>;
+export const Text = ({
+    size = '14',
+    color = 'black',
+    type = 'p',
+    bold,
+    children,
+}: Props) => {
+    return (
+        <Wrapper as={type as any} bold={bold} size={size} color={color}>
+            {children}
+        </Wrapper>
+    );
 };
 
 export default Text;
