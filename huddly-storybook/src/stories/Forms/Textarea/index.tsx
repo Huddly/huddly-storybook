719b44bed954c9eaa3961ca54fd7ffc5a963d003
RostiMelk <hello@rosti.no>
2022-02-08 12:47:24 +0100

    Update comments

--- huddly-storybook/src/stories/Forms/Textarea/index.tsx
+++ huddly-storybook/src/stories/Forms/Textarea/index.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 interface TextAreaProps {
     /**
-     * label description.
+     * Label text
      */
     label?: string;
 
     /**
-     * isRequired description.
+     * Is input required?
      */
     isRequired?: boolean;
 
