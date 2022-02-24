571d029a586ab3072cc54081535650ca992f91b8
RostiMelk <hello@rosti.no>
2022-02-24 12:42:01 +0100

    Icon component

---
+++ src/stories/Foundation/Icon/Icon.stories.tsx
@@ -0,0 +1,11 @@
+import { Icon } from '.';
+
+export default { component: Icon };
+
+export const Primary = {
+  args: {
+    icon: 'Sample string',
+    size: 'Sample string',
+    color: 'Sample string',
+  },
+};
---
+++ src/stories/Foundation/Icon/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  /* Your styles here */
+`;
+
+export interface IconProps {
+  className?: string;
+  icon: any;
+  size: any;
+  color: any;
+}
+
+/**
+ * Icon component
+ */
+export const Icon = ({ className, icon, size, color }: IconProps) => {
+  return <Wrapper className={className}>Icon</Wrapper>;
+};
