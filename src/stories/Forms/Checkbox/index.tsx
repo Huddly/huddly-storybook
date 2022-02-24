4774aae53081e62abae4ef609587e4c44c92d658
RostiMelk <hello@rosti.no>
2022-02-24 09:58:40 +0100

    Add custom checkbox style

--- src/stories/Forms/Checkbox/index.tsx
+++ src/stories/Forms/Checkbox/index.tsx
@@ -7,26 +7,47 @@ interface WrapperProps {
 }
 
 const Wrapper = styled.div<WrapperProps>`
-  display: flex;
-  flex-wrap: wrap;
-  column-gap: var(--spacing-8);
-
-  input[type='checkbox'] {
-    flex-shrink: 0;
-    width: var(--spacing-16);
-    height: var(--spacing-16);
-    margin: 0;
-    cursor: pointer;
-    accent-color: var(--color-lavender);
-  }
-
   label {
-    width: calc(100% - var(--spacing-24)); // Substract checkbox size and gap
+    display: flex;
     font-size: var(--font-size-14);
     cursor: pointer;
 
+    // Checkbox
+    &:before {
+      display: block;
+      flex-shrink: 0;
+      width: var(--spacing-16);
+      height: var(--spacing-16);
+      margin-right: var(--spacing-8);
+      border: var(--border-primary);
+      border-radius: 2px;
+      content: '';
+    }
+
     a {
       color: var(--color-lavender);
+    }
+  }
+
+  input[type='checkbox'] {
+    opacity: 0;
+    position: absolute;
+    left: -99999px; // This is to hide the checkbox without affecting screen readers
+
+    // Focus ring
+    &:focus + label:before {
+      outline: 1px dotted #212121; // Fallback to non-webkit browsers
+      outline: 5px auto -webkit-focus-ring-color;
+      outline-offset: 3px;
+    }
+
+    // Checked checkbox style
+    &:checked + label:before {
+      border-color: var(--color-lavender);
+      background-color: var(--color-lavender);
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='16px' viewBox='0 0 24 24' width='16px' fill='%23fff'%3E%3Cpath d='M0 0h24v24H0V0z' fill='none'/%3E%3Cpath d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/%3E%3C/svg%3E");
+      background-position: center;
+      background-size: cover;
     }
   }
 `;
@@ -69,7 +90,9 @@ export const Checkbox = React.forwardRef(
           {...additionalInputProps}
         />
 
-        <label htmlFor={id}>{children}</label>
+        <label htmlFor={id}>
+          <span>{children}</span>
+        </label>
       </Wrapper>
     );
   }
