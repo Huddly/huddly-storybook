a60baeb3705dba45a4333d0c37dcf1f8addb7f97
Andrea Marie Festøy <andreamarie@huddly.com>
2022-03-14 13:05:18 +0100

    bug: fix card type

--- src/stories/Foundation/Card/index.tsx
+++ src/stories/Foundation/Card/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export interface CardProps {
   background?: Colors;
-  children: JSX.Element | JSX.Element[];
+  children: React.ReactNode;
   className?: string;
 }
 
