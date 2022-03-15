bb59110e662e7825c8d16173dfadb1b0668419d4
Rostislav Melkumyan <hello@rosti.no>
2022-03-15 10:46:11 +0100

    Feat(Fonts): Added woff and eot font files, and italic variations (#28)

--- src/shared/global.ts
+++ src/shared/global.ts
@@ -73,16 +73,42 @@ export const GlobalStyles = createGlobalStyle`
   body {
     font-family: var(--font-family-primary);
   }
+
+  /* Import fonts */
   @font-face {
     font-family: "Messina sans";
-    src: url("/fonts/Messina-sans/MessinaSansWeb-Bold.woff2") format('woff2');
+    src: url("/fonts/Messina-sans/MessinaSansWeb-Regular.woff2") format('woff2'),
+      url("/fonts/Messina-sans/MessinaSansWeb-Regular.woff") format('woff'),
+      url("/fonts/Messina-sans/MessinaSansWeb-Regular.eot") format('embedded-opentype');
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Messina sans";
+    src: url("/fonts/Messina-sans/MessinaSansWeb-RegularItalic.woff2") format('woff2'),
+      url("/fonts/Messina-sans/MessinaSansWeb-RegularItalic.woff") format('woff'),
+      url("/fonts/Messina-sans/MessinaSansWeb-RegularItalic.eot") format('embedded-opentype');
+    font-style: italic;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Messina sans";
+    src: url("/fonts/Messina-sans/MessinaSansWeb-Bold.woff2") format('woff2'),
+      url("/fonts/Messina-sans/MessinaSansWeb-Bold.woff") format('woff'),
+      url("/fonts/Messina-sans/MessinaSansWeb-Bold.eot") format('embedded-opentype');
+    font-style: normal;
     font-weight: 700;
     font-display: swap;
   }
   @font-face {
     font-family: "Messina sans";
-    src: url("/fonts/Messina-sans/MessinaSansWeb-Regular.woff2") format('woff2');
-    font-weight: 400;
+    src: url("/fonts/Messina-sans/MessinaSansWeb-BoldItalic.woff2") format('woff2'),
+      url("/fonts/Messina-sans/MessinaSansWeb-BoldItalic.woff") format('woff'),
+      url("/fonts/Messina-sans/MessinaSansWeb-BoldItalic.eot") format('embedded-opentype');
+    font-style: italic;
+    font-weight: 700;
     font-display: swap;
   }
 `;
