fe0b27ae456ae1f256d9883ff86b924b9d0de24d
Andrea Marie Festøy <andreamarie@huddly.com>
2022-06-15 10:14:20 +0200

    feat: Tooltip component

--- src/stories/Foundation/IconButton/index.tsx
+++ src/stories/Foundation/IconButton/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Flex } from '../Flex';
 import styled from 'styled-components';
 import { Colors } from '../../../shared/colors';
 import { Icon, Icons } from '../Icon';
 import { Spinner } from '../Spinner';
-import { Text } from '../Text';
+import { Tooltip } from '../Tooltip';
 
 const ButtonElement = styled.button<{ color: Colors }>`
   cursor: pointer;
@@ -27,28 +26,6 @@ const ButtonElement = styled.button<{ color: Colors }>`
   path {
     fill: ${(p) => `var(--color-${p.color})`};
   }
-`;
-
-const ToolTip = styled(Flex)`
-  display: none;
-  position: absolute;
-  left: 50%;
-  top: -135%;
-  transform: translateX(-50%);
-`;
-
-const ToolTipText = styled(Text)`
-  background: black;
-  padding: var(--spacing-4) var(--spacing-8);
-  border-radius: 2px;
-`;
-
-const Triangle = styled.span`
-  width: 0;
-  height: 0;
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-top: 5px solid black;
 `;
 
 export interface IconButtonProps {
@@ -75,7 +52,7 @@ export const IconButton = ({
   icon,
   tooltipText,
 }: IconButtonProps) => {
-  return (
+  const Button = () => (
     <ButtonElement
       aria-busy={loading}
       className={className}
@@ -85,17 +62,17 @@ export const IconButton = ({
       type={type}
     >
       {loading && <Spinner size={24} />}
-      {!loading && (
-        <>
-          <Icon name={icon} />
-          {tooltipText && (
-            <ToolTip direction='column'>
-              <ToolTipText color='white'>{tooltipText}</ToolTipText>
-              <Triangle />
-            </ToolTip>
-          )}
-        </>
-      )}
+      {!loading && <Icon name={icon} />}
     </ButtonElement>
+  );
+
+  if (!tooltipText) {
+    return <Button />;
+  }
+
+  return (
+    <Tooltip text={tooltipText}>
+      <Button />
+    </Tooltip>
   );
 };
