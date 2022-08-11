78987c4fe3d9db4fe2174e32f75fa10f98910605
RostiMelk <hello@rosti.no>
2022-08-11 13:28:35 +0200

    revert changes from other branch

--- src/stories/Foundation/Tooltip/Tooltip.stories.tsx
+++ src/stories/Foundation/Tooltip/Tooltip.stories.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import { Tooltip } from '.';
 import { Button } from '../../Forms/Button';
@@ -6,34 +5,14 @@ import { Button } from '../../Forms/Button';
 export default { component: Tooltip };
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
   margin-top: var(--spacing-32);
 `;
 
 const BasicTemplate = () => {
-  const [veryCount, setVeryCount] = useState(1);
-
-  const getRandomMore = () => {
-    const very = ['More', 'Much', 'So much', 'Very much', 'Moaaarr'];
-    return very[Math.floor(Math.random() * very.length)];
-  };
-
-  const getTooltipText = () => {
-    const veryStr = Array(veryCount).fill('very').join(' ');
-    return `A ${veryStr} cool tooltip! ✨`;
-  };
-
-  const addVery = () => {
-    setVeryCount(veryCount + 1);
-  };
-
-  const tooltipText = getTooltipText();
-  const buttonText = getRandomMore();
   return (
     <Wrapper>
-      <Tooltip text={tooltipText}>
-        <Button onClick={addVery}>{buttonText} very</Button>
+      <Tooltip text='Click me'>
+        <Button onClick={() => console.log('click')}>Button</Button>
       </Tooltip>
     </Wrapper>
   );
