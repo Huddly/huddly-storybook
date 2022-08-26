2dc8a7d543418a81762a06e94a26bf7b052aed4c
Andrea Marie Festøy <andreamarie@huddly.com>
2022-08-26 12:20:18 +0200

    feat: Change checkbox styling

--- src/stories/Forms/Checkbox/index.tsx
+++ src/stories/Forms/Checkbox/index.tsx
@@ -38,7 +38,7 @@ const Wrapper = styled.div<WrapperProps>`
     left: ${rem(-99999)}; // This is to hide the checkbox without affecting screen readers
 
     // Focus ring
-    &:focus + label:before {
+    &:focus-visible + label:before {
       outline: ${rem(1)} dotted #212121; // Fallback to non-webkit browsers
       outline: ${rem(5)} auto -webkit-focus-ring-color;
       outline-offset: ${rem(3)};
@@ -48,10 +48,29 @@ const Wrapper = styled.div<WrapperProps>`
     &:checked + label:before {
       border-color: var(--color-lavender);
       background-color: var(--color-lavender);
-      background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg enable-background='new 0 0 533.973 533.973' version='1.1' viewBox='0 0 533.97 533.97' xml:space='preserve' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='%23fff' d='m477.93 52.261c-12.821-12.821-33.605-12.821-46.427 0l-266.96 266.95-62.075-62.069c-12.821-12.821-33.604-12.821-46.426 0l-46.427 46.426c-12.821 12.821-12.821 33.604 0 46.426l131.72 131.72c12.821 12.821 33.611 12.821 46.426 0l336.6-336.6c12.821-12.821 12.821-33.605 0-46.426l-46.425-46.426z'/%3E%3C/svg%3E%0A");
+      background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg enable-background='new 0 0 14 10' version='1.1' viewBox='0 0 14 10' xml:space='preserve' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='%23fff' d='M5.56904 8.7643L6.27614 9.47141L6.27614 9.4714L5.56904 8.7643ZM5.09763 8.7643L5.80474 8.05719L5.80474 8.05719L5.09763 8.7643ZM6.27614 9.4714L13.7071 2.04044L12.2929 0.626226L4.86193 8.05719L6.27614 9.4714ZM5.80474 8.05719L1.70711 3.95956L0.292893 5.37377L4.39052 9.4714L5.80474 8.05719ZM4.86193 8.05719C5.12228 7.79684 5.54439 7.79684 5.80474 8.05719L4.39052 9.4714C4.91122 9.9921 5.75544 9.9921 6.27614 9.47141L4.86193 8.05719Z'/%3E%3C/svg%3E%0A");
       background-repeat: no-repeat;
       background-position: center;
-      background-size: cover;
+    }
+
+    &:hover:checked + label:before {
+      background-color: var(--color-salviaBlue);
+      border-color: var(--color-salviaBlue);
+    }
+
+    &:hover:not(:checked) + label:before {
+      background-color: var(--color-grey91);
+      border-color: var(--color-grey45);
+    }
+
+    &:active:checked + label:before {
+      background-color: var(--color-royalBlue);
+      border-color: var(--color-royalBlue);
+    }
+
+    &:active:not(:checked) + label:before {
+      background-color: var(--color-grey86);
+      border-color: var(--color-grey35);
     }
   }
 `;
