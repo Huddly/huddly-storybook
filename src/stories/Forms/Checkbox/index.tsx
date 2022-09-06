e65d9c96d801a253335eab54813d8330b2a631b3
RostiMelk <hello@rosti.no>
2022-09-06 12:46:45 +0200

    refactor to use styled-components

--- src/stories/Forms/Checkbox/index.tsx
+++ src/stories/Forms/Checkbox/index.tsx
@@ -3,80 +3,74 @@ import styled from 'styled-components';
 import rem from '../../../shared/pxToRem';
 import { GlobalInputProps } from '../../../shared/types';
 
-interface WrapperProps {
-  hasError?: boolean;
-}
+const FakeCheckbox = styled.label<{ hasError: boolean }>`
+  display: flex;
+  position: relative;
+  align-items: center;
+  color: var(--color-grey15);
+  font-size: var(--font-size-14);
+  cursor: pointer;
 
-const Wrapper = styled.div<WrapperProps>`
-  label {
-    display: flex;
-    position: relative;
-    align-items: center;
-    color: var(--color-grey15);
-    font-size: var(--font-size-14);
-    cursor: pointer;
-
-    // Checkbox
-    &:before {
-      display: block;
-      box-sizing: border-box;
-      flex-shrink: 0;
-      width: var(--spacing-24);
-      height: var(--spacing-24);
-      border-radius: ${rem(2)};
-      content: '';
-      border: ${({ hasError }) =>
-        hasError ? 'var(--border-error)' : `${rem(2)} solid var(--color-grey55);`};
-    }
-
-    span {
-      margin-left: var(--spacing-8);
-    }
-
-    a {
-      color: var(--color-lavender);
-    }
+  // The fake custom checkbox
+  &:before {
+    display: block;
+    flex-shrink: 0;
+    box-sizing: border-box;
+    width: var(--spacing-24);
+    height: var(--spacing-24);
+    border: ${({ hasError }) =>
+      hasError ? 'var(--border-error)' : `${rem(2)} solid var(--color-grey55);`};
+    border-radius: ${rem(2)};
+    content: '';
   }
 
-  input[type='checkbox'] {
-    opacity: 0;
-    position: absolute;
-    left: ${rem(-99999)}; // This is to hide the checkbox without affecting screen readers
-
-    // Focus ring
-    &:focus-within + label:before {
-      box-shadow: 0px 0px 0px ${rem(2)} white, 0px 0px 0px ${rem(4)} var(--color-lavender);
-    }
-
-    // Checked checkbox style
-    &:checked + label:before {
-      border-color: var(--color-lavender);
-      background-color: var(--color-lavender);
-      background-image: url("data:image/svg+xml,%3Csvg width='12' height='10' viewBox='0 0 12 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11.25 1.25L4.5 8L0.75 4.25' stroke='white' stroke-width='2'/%3E%3C/svg%3E%0A");
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-
-    &:hover:checked + label:before {
-      background-color: var(--color-salviaBlue);
-      border-color: var(--color-salviaBlue);
-    }
-
-    &:hover:not(:checked) + label:before {
-      background-color: var(--color-grey91);
-      border-color: var(--color-grey45);
-    }
-
-    &:active:checked + label:before {
-      background-color: var(--color-royalBlue);
-      border-color: var(--color-royalBlue);
-    }
-
-    &:active:not(:checked) + label:before {
-      background-color: var(--color-grey86);
-      border-color: var(--color-grey35);
-    }
+  a {
+    color: var(--color-lavender);
   }
+`;
+
+const CheckboxInput = styled.input.attrs({ type: 'checkbox' })<GlobalInputProps>`
+  opacity: 0;
+  position: absolute;
+  left: ${rem(-99999)}; // This is to hide the checkbox without affecting screen readers
+
+  // Focus ring
+  &:focus-within + ${FakeCheckbox}:before {
+    box-shadow: 0px 0px 0px ${rem(2)} white, 0px 0px 0px ${rem(4)} var(--color-lavender);
+  }
+
+  // Checked checkbox style
+  &:checked + ${FakeCheckbox}:before {
+    border-color: var(--color-lavender);
+    background-color: var(--color-lavender);
+    background-image: url("data:image/svg+xml,%3Csvg width='12' height='10' viewBox='0 0 12 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11.25 1.25L4.5 8L0.75 4.25' stroke='white' stroke-width='2'/%3E%3C/svg%3E%0A");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  &:hover:checked + ${FakeCheckbox}:before {
+    border-color: var(--color-salviaBlue);
+    background-color: var(--color-salviaBlue);
+  }
+
+  &:hover:not(:checked) + ${FakeCheckbox}:before {
+    border-color: var(--color-grey45);
+    background-color: var(--color-grey91);
+  }
+
+  &:active:checked + ${FakeCheckbox}:before {
+    border-color: var(--color-royalBlue);
+    background-color: var(--color-royalBlue);
+  }
+
+  &:active:not(:checked) + ${FakeCheckbox}:before {
+    border-color: var(--color-grey35);
+    background-color: var(--color-grey86);
+  }
+`;
+
+const LabelText = styled.span`
+  margin-left: var(--spacing-8);
 `;
 
 export interface CheckboxProps extends Omit<GlobalInputProps, 'value'> {
@@ -103,8 +97,8 @@ export const Checkbox = React.forwardRef(
     } = props;
 
     return (
-      <Wrapper className={className} hasError={hasError}>
-        <input
+      <div className={className}>
+        <CheckboxInput
           aria-labelledby={ariaDescribedBy}
           aria-errormessage={ariaErrorMessage}
           aria-invalid={hasError}
@@ -116,8 +110,10 @@ export const Checkbox = React.forwardRef(
           type='checkbox'
           {...additionalInputProps}
         />
-        <label htmlFor={id}>{children && <span>{children}</span>}</label>
-      </Wrapper>
+        <FakeCheckbox htmlFor={id} hasError={hasError}>
+          {children && <LabelText>{children}</LabelText>}
+        </FakeCheckbox>
+      </div>
     );
   }
 );
