9642819b0c01fea5294fd84398ae0dd9979ea183
RostiMelk <hello@rosti.no>
2022-09-08 13:13:40 +0200

    remove unused config code

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,13 @@
-import type { StorybookViteConfig } from '@storybook/builder-vite';
-
-const config: StorybookViteConfig = {
-  framework: '@storybook/react',
+module.exports = {
   stories: ['../src/stories/**/*.stories.mdx', '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
     disableTelemetry: true,
   },
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-    return config;
-  },
 };
-
-export default config;
