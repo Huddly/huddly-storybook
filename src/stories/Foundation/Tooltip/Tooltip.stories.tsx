d1d47b0365ee82dc282ac73e89ab325a00ab9506
RostiMelk <hello@rosti.no>
2023-01-30 20:41:28 +0100

    Delete old code and finalize component

--- src/stories/Foundation/Tooltip/Tooltip.stories.tsx
+++ src/stories/Foundation/Tooltip/Tooltip.stories.tsx
@@ -5,19 +5,13 @@ import { Button } from '../../Forms/Button';
 
 export default { component: Tooltip };
 
-const Wrapper = styled.div<{ pushOutOfBounds: boolean }>`
-  display: flex;
+const Wrapper = styled.div`
+  margin-top: var(--spacing-48);
   justify-content: center;
-  margin-top: ${(p) => (p.pushOutOfBounds ? '0' : 'var(--spacing-48)')};
 `;
 
-const Template = ({ align, pushOutOfBounds = true }) => {
+const Template = ({ alignX, alignY }) => {
   const [veryCount, setVeryCount] = useState(1);
-
-  const getRandomMore = () => {
-    const very = ['More', 'Much', 'So much', 'Very much', 'Moaaarr'];
-    return very[Math.floor(Math.random() * very.length)];
-  };
 
   const getTooltipText = () => {
     const veryStr = Array(veryCount).fill('very').join(' ');
@@ -29,32 +23,80 @@ const Template = ({ align, pushOutOfBounds = true }) => {
   };
 
   const tooltipText = getTooltipText();
-  const buttonText = getRandomMore();
   return (
-    <Wrapper pushOutOfBounds={pushOutOfBounds}>
-      <Tooltip text={tooltipText} align={align}>
-        <Button onClick={addVery}>{buttonText} very</Button>
+    <Wrapper>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <Tooltip text={tooltipText} alignX={alignX} alignY={alignY}>
+        <Button onClick={addVery}>dddd</Button>
       </Tooltip>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
     </Wrapper>
   );
 };
 
-export const Left = Template.bind({});
-Left.args = {
-  align: 'left',
-};
-
-export const Center = Template.bind({});
-Center.args = {
-  align: 'center',
-};
-
-export const Right = Template.bind({});
-Right.args = {
-  align: 'right',
-};
-
-export const OutOfBounds = Template.bind({});
-OutOfBounds.args = {
-  pushOutOfBounds: true,
+export const Primary = Template.bind({});
+Primary.args = {
+  alignX: 'center',
+  alignY: 'top',
 };
--- src/stories/Foundation/Tooltip/index.tsx
+++ src/stories/Foundation/Tooltip/index.tsx
@@ -1,58 +1,48 @@
-import React, { useState, useRef, useEffect, useId } from 'react';
+import React, { useState, useRef, useEffect, useId, useCallback } from 'react';
 import styled from 'styled-components';
 import { Text } from '../Text';
 import rem from '../../../shared/pxToRem';
+import { AlignX, AlignY, alignMap, arrowAlignMap } from './alignments';
 
 const Wrapper = styled.div`
   position: relative;
+  display: inline-block;
 `;
 
 const ToolTipWrapper = styled.div<{
+  alignX: AlignX;
+  alignY: AlignY;
   visible: boolean;
-  horizontalAlign: string;
-  verticalAlign: string;
 }>`
+  --gap: ${rem(10)};
   display: flex;
   visibility: ${(p) => (p.visible ? 'visible' : 'hidden')};
   position: absolute;
-  left: ${(p) =>
-    p.horizontalAlign === 'left' ? 0 : p.horizontalAlign === 'center' ? '50%' : 'auto'};
-  right: ${(p) => (p.horizontalAlign === 'right' ? 0 : 'auto')};
-  justify-content: ${(p) =>
-    p.horizontalAlign === 'left'
-      ? 'flex-start' // Left
-      : p.horizontalAlign === 'right'
-      ? 'flex-end' // Right
-      : 'center'}; // Center
   align-items: center;
-  width: max-content;
-  max-width: 400px;
-  transform: translateY(
-      ${(p) => (p.verticalAlign === 'top' ? 'calc(-100% - 10px)' : 'calc(100% + 10px)')}
-    )
-    translateX(${(p) => (p.horizontalAlign === 'center' ? '-50%' : '0')});
+  // clamp auto width, max 400px but no more than 80vw
+  width: clamp(1px, 80vw, ${rem(400)});
+  ${(p) => alignMap[p.alignY][p.alignX]}
 
+  // Tooltip arrow
   &:before {
+    --height: ${rem(8)};
     position: absolute;
-    top: ${(p) => (p.verticalAlign === 'top' ? '100%' : 'auto')};
-    bottom: ${(p) => (p.verticalAlign === 'bottom' ? '100%' : 'auto')};
-    transform: ${(p) => (p.verticalAlign === 'top' ? 'rotate(0deg)' : 'rotate(180deg)')};
     width: 0;
     height: 0;
-    border-top: 5px solid black;
-    border-right: 5px solid transparent;
-    border-left: 5px solid transparent;
+    border-width: ${rem(8)} ${rem(6.5)} 0 ${rem(6.5)};
+    border-style: solid;
+    border-color: var(--color-black) transparent transparent transparent;
     content: '';
-    margin-right: 12px;
-    margin-left: 12px;
+    margin-inline: ${(p) => p.alignY === 'center' && 'calc(var(--height) * -1)'};
+    ${(p) => arrowAlignMap[p.alignY][p.alignX]}
   }
 `;
 
 const ToolTipText = styled(Text)`
   position: relative;
-  background: black;
+  background: var(--color-black);
   padding: 6px 12px;
-  border-radius: 2px;
+  border-radius: 3px;
 `;
 
 export interface Props {
@@ -62,168 +52,110 @@ export interface Props {
   /**
    * @default 'center'
    */
-  align?: 'left' | 'right' | 'center';
+  alignX?: AlignX;
+  /**
+   * @default 'top'
+   */
+  alignY?: AlignY;
 }
 
 /**
  * Tooltip component
  */
-export const Tooltip = ({ children, className, text, align = 'center' }: Props) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const [verticalAlign, setVerticalAlign] = useState<'top' | 'bottom'>('bottom');
+export const Tooltip = ({
+  children,
+  className,
+  text,
+  alignX = 'center',
+  alignY = 'top',
+}: Props) => {
+  // alignX and alignY cannot be both 'center'
+  if (alignX === 'center' && alignY === 'center') {
+    alignY = 'top';
+    console.warn(
+      'Tooltip: alignX and alignY cannot be both "center". alignY has been set to "top".'
+    );
+  }
+
   const tooltipId = useId();
   const tooltipRef = useRef<HTMLDivElement>(null);
-  const triggerRef = useRef<HTMLSpanElement>(null);
 
-  useEffect(() => {
-    if (!tooltipRef.current || !triggerRef.current) return;
+  const [isVisible, setIsVisible] = useState(false);
+  const [localAlignX, setLocalAlignX] = useState<AlignX>(alignX);
+  const [localAlignY, setLocalAlignY] = useState<AlignY>(alignY);
 
-    const tooltip = tooltipRef.current;
-    const trigger = triggerRef.current;
+  const handleAlignReset = useCallback(() => {
+    setLocalAlignX(alignX);
+    setLocalAlignY(alignY);
+  }, [alignX, alignY]);
 
-    if (document.querySelector('svg')) {
-      document.querySelector('svg').remove();
+  const handleAlignIfOutOfViewport = () => {
+    // Check if tooltip is outside of viewport
+    const tooltipRect = tooltipRef.current.getBoundingClientRect();
+    // When alignY is center, the tooltip is displayed inline with the trigger
+    const inline = localAlignY === 'center';
+
+    const leftOrRight = () => {
+      if (tooltipRect.left < 0) {
+        setLocalAlignX(inline ? 'right' : 'left');
+      } else if (tooltipRect.right > window.innerWidth) {
+        setLocalAlignX(inline ? 'left' : 'right');
+      }
+    };
+
+    const topOrBottom = () => {
+      if (tooltipRect.top < 0) {
+        setLocalAlignY('bottom');
+      } else if (tooltipRect.bottom > window.innerHeight) {
+        setLocalAlignY('top');
+      }
+    };
+
+    if (localAlignY === 'center') {
+      leftOrRight();
+    } else {
+      leftOrRight();
+      topOrBottom();
     }
-
-    const tooltipSize = {
-      x: tooltip.offsetWidth,
-      y: tooltip.offsetHeight,
-    };
-    const triggerSize = {
-      x: trigger.offsetWidth,
-      y: trigger.offsetHeight,
-    };
-    const pathHeight =
-      trigger.getBoundingClientRect().bottom -
-      tooltip.getBoundingClientRect().bottom +
-      tooltipSize.y;
-
-    const mousePath = `M0 ${_verticalAlign === 'top' ? 0 : pathHeight} 
-          L${tooltipSize.x} ${_verticalAlign === 'top' ? 0 : pathHeight}
-          L${tooltipSize.x} ${tooltipSize.y}
-          L${
-            align === 'center'
-              ? (tooltipSize.x - triggerSize.x) / 2 + triggerSize.x - 10 // center align
-              : align === 'right'
-              ? tooltipSize.x // right align
-              : triggerSize.x // left align
-          } ${_verticalAlign === 'top' ? pathHeight : 0}
-          L${
-            align === 'center'
-              ? (tooltipSize.x - triggerSize.x) / 2 + 10 // center align
-              : align === 'right'
-              ? tooltipSize.x - triggerSize.x // right align
-              : 0 // left align
-          } ${_verticalAlign === 'top' ? pathHeight : 0}
-          L0 ${tooltipSize.y}
-          Z`;
-  }, [isVisible]);
+  };
 
   useEffect(() => {
-    if (!tooltipRef.current || !triggerRef.current) return;
+    if (!tooltipRef.current) return;
+    const wrapper = tooltipRef.current.parentElement;
 
-    const tooltip = tooltipRef.current;
-    const trigger = triggerRef.current;
-
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-    let visibilityTimeout: ReturnType<typeof setTimeout>;
+    let enterTimeout: ReturnType<typeof setTimeout>;
+    let leaveTimeout: ReturnType<typeof setTimeout>;
 
     const mouseEnter = () => {
-      const tooltipSize = {
-        x: tooltip.offsetWidth,
-        y: tooltip.offsetHeight,
-      };
-      const triggerSize = {
-        x: trigger.offsetWidth,
-        y: trigger.offsetHeight,
-      };
-      const pathHeight =
-        trigger.getBoundingClientRect().bottom -
-        tooltip.getBoundingClientRect().bottom +
-        tooltipSize.y;
-
-      svg.style.position = 'absolute';
-      svg.style.right = align === 'right' ? '0' : 'auto';
-      svg.style.left = align === 'left' ? '0' : align === 'center' ? '50%' : 'auto';
-      svg.style.width = `${tooltipSize.x}px`;
-      svg.style.height = `${pathHeight}px`;
-      svg.style.pointerEvents = 'none';
-      svg.style.transform = `translateX(${align === 'center' ? '-50%' : '0'})`;
-
-      path.style.fill = 'none';
-      path.style.pointerEvents = 'all';
-      path.style.stroke = 'red';
-      path.style.strokeWidth = '2px';
-      path.style.strokeDasharray = '5, 5';
-
-      svg.appendChild(path);
-      tooltip.parentElement.prepend(svg);
-
-      // Calculate vertical align, align bottom if tooltip is too close to the top, else align top
-      // calculate the top value by using our exisiting tooltip and trigger elements
-      const tooltipTop = tooltipSize.y + triggerSize.y + 10;
-      const windowTop = window.scrollY;
-      const _verticalAlign = tooltipTop + windowTop > window.innerHeight ? 'top' : 'bottom';
-
-      console.log({
-        _verticalAlign,
-      });
-
-      svg.style.top = _verticalAlign === 'top' ? 'auto' : '0';
-      svg.style.bottom = _verticalAlign === 'bottom' ? 'auto' : '0';
-      setVerticalAlign(_verticalAlign);
-
-      const mousePath = `M0 ${_verticalAlign === 'top' ? 0 : pathHeight} 
-          L${tooltipSize.x} ${_verticalAlign === 'top' ? 0 : pathHeight}
-          L${tooltipSize.x} ${tooltipSize.y}
-          L${
-            align === 'center'
-              ? (tooltipSize.x - triggerSize.x) / 2 + triggerSize.x - 10 // center align
-              : align === 'right'
-              ? tooltipSize.x // right align
-              : triggerSize.x // left align
-          } ${_verticalAlign === 'top' ? pathHeight : 0}
-          L${
-            align === 'center'
-              ? (tooltipSize.x - triggerSize.x) / 2 + 10 // center align
-              : align === 'right'
-              ? tooltipSize.x - triggerSize.x // right align
-              : 0 // left align
-          } ${_verticalAlign === 'top' ? pathHeight : 0}
-          L0 ${tooltipSize.y}
-          Z`;
-
-      visibilityTimeout = setTimeout(() => {
+      leaveTimeout ? clearTimeout(leaveTimeout) : handleAlignIfOutOfViewport();
+      enterTimeout = setTimeout(() => {
         setIsVisible(true);
-
-        path.setAttribute('d', mousePath);
       }, 200);
     };
 
     const mouseLeave = () => {
-      clearTimeout(visibilityTimeout);
-      setIsVisible(false);
-      setVerticalAlign('top');
-      path.removeAttribute('d');
+      clearTimeout(enterTimeout);
+      leaveTimeout = setTimeout(() => {
+        handleAlignReset();
+        setIsVisible(false);
+      }, 200);
     };
 
-    trigger.addEventListener('mouseenter', mouseEnter);
-    tooltip.parentElement.addEventListener('mouseleave', mouseLeave);
-
+    wrapper.addEventListener('mouseenter', mouseEnter);
+    wrapper.addEventListener('mouseleave', mouseLeave);
     return () => {
-      trigger.removeEventListener('mouseenter', mouseEnter);
-      tooltip.parentElement.removeEventListener('mouseleave', mouseLeave);
-      svg.remove();
+      wrapper.removeEventListener('mouseenter', mouseEnter);
+      wrapper.removeEventListener('mouseleave', mouseLeave);
     };
-  }, [text, children, align]);
+  }, [text, children, localAlignX, localAlignY]);
+
+  console.log('render');
 
   return (
     <Wrapper className={className} role='tooltip'>
       <ToolTipWrapper
-        horizontalAlign={align}
-        verticalAlign={verticalAlign}
+        alignX={localAlignX}
+        alignY={localAlignY}
         aria-hidden={!isVisible}
         id={tooltipId}
         ref={tooltipRef}
@@ -233,9 +165,7 @@ export const Tooltip = ({ children, className, text, align = 'center' }: Props) 
         <ToolTipText color='white'>{text}</ToolTipText>
       </ToolTipWrapper>
 
-      <span ref={triggerRef} aria-describedby={tooltipId}>
-        {children}
-      </span>
+      <span aria-describedby={tooltipId}>{children}</span>
     </Wrapper>
   );
 };
